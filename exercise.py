31537c453ba9841d187c0055884a08bc145be5ab
Daiki1991 <45293899+Daiki1991@users.noreply.github.com>
2020-09-12 18:17:24 +0200

    Create exercise.py

---
+++ exercise.py
@@ -0,0 +1,7 @@
+def total_names_motorcycle(names)
+   new_list = []
+   for name in names:
+     new_list.append("The best" + name)
+     return new_list
+   print(total_names_motorcycle(["Yamaha","Ducati","Honda","Suzuki"]))
+   
